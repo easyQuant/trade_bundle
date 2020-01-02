3b49878c7e1fb5cb4457873fa92ff3e291ad2e8a
easyQuant <532894539@qq.com>
2020-01-02 11:03:51 +0800

    增加pypi支持

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'trade_bundle',      
+    version = '1.1.0',
+    py_modules = ['trade_bundle'],
+    author = 'easyQuant',        
+    author_email = '532894539@qq.com',
+    url = 'https://github.com/easyQuant/trade_bundle',
+    description = 'python版本的 A股实时tick数据源'
+)
